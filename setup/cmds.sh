1aa7aadc43cd3a5bd7b3cb50864ad9c8fd487ebb
Matthew Brett <matthew.brett@gmail.com>
2026-08-22 16:17:46 +0100

    Update commands

--- setup/cmds.sh
+++ setup/cmds.sh
@@ -13,18 +13,23 @@ git annex initremote gdrive-public-hash \
   encryption=none \
   rclone_layout=nodir
 
-git annex initremote gdrive-private-tree \
-  type=rclone \
-  target=gdrive-private \
-  prefix=ga-private-tree \
-  encryption=none \
-  rclone_layout=nodir \
-  exporttree=yes
+# Tree views - do not currently work.
+# Waiting on full rclone exporttree support.
+# git annex initremote gdrive-private-tree \
+#   type=rclone \
+#   target=gdrive-private \
+#   prefix=ga-private-tree \
+#   encryption=none \
+#   rclone_layout=nodir \
+#   exporttree=yes
+# 
+# git annex initremote gdrive-public-tree \
+#   type=rclone \
+#   target=gdrive-public \
+#   prefix=ga-public-tree \
+#   encryption=none \
+#   rclone_layout=nodir \
+#   exporttree=yes
 
-git annex initremote gdrive-public-tree \
-  type=rclone \
-  target=gdrive-public \
-  prefix=ga-public-tree \
-  encryption=none \
-  rclone_layout=nodir \
-  exporttree=yes
+git annex wanted gdrive-private-hash   "include=private/*"
+git annex wanted gdrive-public-hash    "include=public/*"
